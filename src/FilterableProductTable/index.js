1b1be285f7bd438fd0c98ec98b4c55f15fc02a53
haibin@k590s-i7 <894457272@qq.com>
2021-07-12 00:25:01 +0800

    23423423

--- src/FilterableProductTable/index.js
+++ src/FilterableProductTable/index.js
@@ -5,12 +5,42 @@ import ProductTable from './ProductTable';
 class FilterableProductTable extends React.Component {
   static defaultProps = {
     products: [
-      { category: 'Sporting Goods', price: '$49.99', stocked: true, name: 'Football' },
-      { category: 'Sporting Goods', price: '$9.99', stocked: true, name: 'Baseball' },
-      { category: 'Sporting Goods', price: '$29.99', stocked: false, name: 'Basketball' },
-      { category: 'Electronics', price: '$99.99', stocked: true, name: 'iPod Touch' },
-      { category: 'Electronics', price: '$399.99', stocked: false, name: 'iPhone 5' },
-      { category: 'Electronics', price: '$199.99', stocked: true, name: 'Nexus 7' }
+      { 
+        category: 'Sporting Goods', 
+        price: '$49.99', 
+        stocked: true, 
+        name: 'Football' 
+      },
+      { 
+        category: 'Sporting Goods', 
+        price: '$9.99', 
+        stocked: true, 
+        name: 'Baseball' 
+      },
+      { 
+        category: 'Sporting Goods', 
+        price: '$29.99', 
+        stocked: false, 
+        name: 'Basketball' 
+      },
+      { 
+        category: 'Electronics', 
+        price: '$99.99', 
+        stocked: true, 
+        name: 'iPod Touch' 
+      },
+      { 
+        category: 'Electronics', 
+        price: '$399.99', 
+        stocked: false, 
+        name: 'iPhone 5' 
+      },
+      { 
+        category: 'Electronics', 
+        price: '$199.99', 
+        stocked: true, 
+        name: 'Nexus 7' 
+      }
     ],
   }
 
@@ -30,30 +60,51 @@ class FilterableProductTable extends React.Component {
     });
   }
   render() {
-    let filterText = this.state.filterText;
-    const inStockOnly = this.state.inStockOnly;
-    const products = this.props.products.reduce((res, cur, index) => {
-      if (filterText === '') {
-        if (inStockOnly) {
+    const products = this.props.products.filter((cur,index,arr)=>{
+      if (this.state.filterText === '') {
+        if (this.state.inStockOnly) {
           if (cur.stocked) {
-            res.push(cur)
+            return true
           }
-        } else {
-          res.push(cur)
-        }
+          return false
+        } 
+        return true
       } else {
-        if (cur.name.includes(filterText)) {
-          if (inStockOnly) {
+        if (cur.name.includes(this.state.filterText)) {
+          if (this.state.inStockOnly) {
             if (cur.stocked) {
-              res.push(cur)
+              return true
             }
+            return false
           } else {
-            res.push(cur)
+            return true
           }
         }
       }
-      return res
-    }, [])
+    })
+    // const products = this.props.products.reduce((res, cur, index) => {
+    //   if (this.state.filterText === '') {
+    //     if (this.state.inStockOnly) {
+    //       if (cur.stocked) {
+    //         res.push(cur)
+    //       }
+    //     } else {
+    //       res.push(cur)
+    //     }
+    //   } else {
+    //     if (cur.name.includes(this.state.filterText)) {
+    //       if (this.state.inStockOnly) {
+    //         if (cur.stocked) {
+    //           res.push(cur)
+    //         }
+    //       } else {
+    //         res.push(cur)
+    //       }
+    //     }
+    //   }
+    //   return res
+    // }, [])
+
     return (
       <div>
         <SearchBar
